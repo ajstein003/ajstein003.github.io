8d1df4573dfff9955678a67a59343da421dfd900
Arjab Chakrabarti <124612257+ajstein003@users.noreply.github.com>
2023-06-17 14:21:44 +0530

    script.js

--- script.js
+++ script.js
@@ -1,39 +1,35 @@
 const burger = document.querySelector("nav svg");
+const links = document.querySelector(".links");
+const lines = document.querySelectorAll(".line");
+
+gsap.set(links, { x: "200%" });
 
 burger.addEventListener("click", () => {
   if (burger.classList.contains("active")) {
-    gsap.to(".links", { x: "100%" });
-    gsap.to(".line", { stroke: "white" });
+    gsap.to(links, { x: "200%", duration: 0.3, onComplete: hideLinks });
+    gsap.to(lines, { stroke: "white" });
     gsap.set("body", { overflow: "auto" });
     gsap.set("body", { overflowX: "hidden" });
   } else {
-    gsap.to(".links", { x: "0%" });
-    gsap.to(".line", { stroke: "black" });
+    showLinks();
+    gsap.to(".links", { x: "0.5px" }); // Adjust the value as per your preference
+    gsap.to(lines, { stroke: "black" });
     gsap.fromTo(
       ".links a",
       { opacity: 0, y: 0 },
       { opacity: 1, y: 20, delay: 0.25, stagger: 0.25 }
     );
+    gsap.set(".links a:not(:last-child)", { marginBottom: "50px" }); // Add gap between links
     gsap.set("body", { overflow: "hidden" });
   }
   burger.classList.toggle("active");
 });
 
-const videos = gsap.utils.toArray(".video");
-gsap.set(videos, { opacity: 0 });
+function showLinks() {
+  gsap.set(links, { x: "200%" });
+  gsap.to(links, { x: "0%", duration: 0.3 });
+}
 
-videos.forEach((video) => {
-  ScrollTrigger.create({
-    trigger: video,
-    start: "top center",
-    end: "bottom center",
-
-    onEnter: () => {
-      gsap.to(video, { opacity: 1 });
-      video.play();
-    },
-    onEnterBack: () => video.play(),
-    onLeave: () => video.pause(),
-    onLeaveBack: () => video.pause(),
-  });
-});
+function hideLinks() {
+  gsap.set(links, { x: "200%" });
+}
